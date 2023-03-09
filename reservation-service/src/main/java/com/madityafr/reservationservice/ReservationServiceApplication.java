8292894f3a100d6841da19f8dbaa21d11892a6a0
M Aditya Fathur Rahman <madityafr3@gmail.com>
2023-03-10 06:49:23 +0700

    Contoh kafk dto base domain

--- reservation-service/src/main/java/com/madityafr/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/madityafr/reservationservice/ReservationServiceApplication.java
@@ -2,7 +2,9 @@ package com.madityafr.reservationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class ReservationServiceApplication {
 
